7782658391c5695f584d42b5f5ae2b5fe9178a68
Bruno Bossola <bruno@meterian.com>
2021-06-15 11:18:34 +0100

    Added mix files

---
+++ mix.exs
@@ -0,0 +1,29 @@
+defmodule Sample.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :sample,
+      version: "0.1.0",
+      elixir: "~> 1.11",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:cowboy, "~> 2.8.0"},
+      {:jason, "~> 1.2.2"}
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+end
